63e50273113eb5f1aa1cb524978e469638459ebd
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-05-20 19:24:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+function shout(string) {
+
+  return string.toUppercase();
+}
+
+function whisper(string) {
+
+ return string.toLowerCase()
+
+}
+
+
+function logShout(string) {
+
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+
+console.log(string.toLowerCase());
+
+}
+
+// function sayHiToGrandma(string) {
+//
+// if (string = "I love you, Grandma.") {
+//   console.log("I love you, too.")
+// }
+//
+// else if (string.toUppercase()) {
+//
+//   console.log("YES INDEED!")
+//
+// } else if (string.toLowerCase()) {
+//
+//   console.log("I can't hear you!")
+// }
