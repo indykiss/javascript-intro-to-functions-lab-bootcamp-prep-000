ed8c69ebd857ffd6660fbc320d6b488950652662
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-05-20 19:28:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,10 @@ function sayHiToGrandma(string) {
   else if (string == string.toUpperCase()) {
     return "YES INDEED!"
   }
-  
+
+  else if (string == "I love you, Grandma.") {
+    return "I love you, too."
+  }
 
 }
 
